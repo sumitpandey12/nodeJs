037b63ae2885836cb51262f23631ce9119227290
sumitpadney12 <heerjnv@gmail.com>
2024-02-07 14:58:03 +0530

    shop route changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+app.use("/shop", shopRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page Not Found</h1>");
