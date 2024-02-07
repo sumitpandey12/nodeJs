98daa78e4a1e7f7b46cc56455fa63677baabb0cb
sumitpadney12 <heerjnv@gmail.com>
2024-02-07 13:56:41 +0530

    Learning about body-parser and post get  method and diffrents types of routing

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const http = require("http");
 const express = require("express");
-
-const routes = require("./routes");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In Middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add_product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Another Middleware");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from express</h1>");
 });
 
